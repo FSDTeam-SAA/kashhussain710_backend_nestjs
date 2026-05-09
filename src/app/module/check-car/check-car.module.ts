001b43276a6ca62c64cc6320a6fd02f8a7fc55d3
saurav11sarkar <sarkarsaurav11@outlook.com>
2026-05-09 09:07:34 +0600

    Refactor CheckCar entity: restructure schema, remove commented code, and enhance type definitions for clarity and maintainability.

--- src/app/module/check-car/check-car.module.ts
+++ src/app/module/check-car/check-car.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from '../user/entities/user.entity';
-import { VehicleReport, VehicleReportSchema } from './entities/check-car.entity';
-import { CheckCarController } from './check-car.controller';
 import { CheckCarService } from './check-car.service';
+import { CheckCarController } from './check-car.controller';
+import { CheckCar, CheckCarSchema } from './entities/check-car.entity';
+
+import { User, UserSchema } from '../user/entities/user.entity';
+import {
+  MotHistory,
+  MotHistorySchema,
+} from '../mot-history/entities/mot-history.entity';
+import {
+  Subscribe,
+  SubscribeSchema,
+} from '../subscribe/entities/subscribe.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-       { name: VehicleReport.name, schema: VehicleReportSchema },
+      { name: CheckCar.name, schema: CheckCarSchema },
+      { name: MotHistory.name, schema: MotHistorySchema },
       { name: User.name, schema: UserSchema },
       { name: Subscribe.name, schema: SubscribeSchema },
     ]),
