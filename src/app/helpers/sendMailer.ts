9b12aa037ae28152333e5a389022fe7a10cffb23
Kongkon-79 <kongkonjowarder79@gmail.com>
2026-06-11 10:33:14 +0600

    company name change

--- src/app/helpers/sendMailer.ts
+++ src/app/helpers/sendMailer.ts
@@ -12,7 +12,7 @@ const sendMailer = async (email: string, subject?: string, html?: string) => {
     },
   });
   const info = await transporter.sendMail({
-    from: `"Carcheckal" ${config.email.from}`,
+    from: `"carcheckai" ${config.email.from}`,
     to: email,
     subject,
     html,
